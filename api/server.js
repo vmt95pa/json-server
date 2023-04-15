6465925e981bdd69c899cb9a272232a775954eef
VUONGMINHTUAN\ADMIN <vuongminhtuan95@gmail.com>
2023-04-15 08:37:27 +0700

    no message

--- api/server.js
+++ api/server.js
@@ -6,10 +6,6 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
